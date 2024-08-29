0802f4528340f0cff0c16630d84e952361f25448
RubiBarragan12 <152565400+RubiBarragan12@users.noreply.github.com>
2024-08-29 17:40:00 -0600

    Add files via upload

---
+++ App3Pilas.java
@@ -0,0 +1,34 @@
+package pilas;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+import pilas.PilaEnlazada;
+/**
+ * Se lee cadena de caracteres , luego se inserta cada caracter
+ * de la cadena en una pila y luego se extraen los caracteres de la pila y
+ * se muestran en la salida.
+ * 
+ * NOTA:La cadena leida en la entrada, se mostrara en la salida
+ *de manera invertiva, por ejemplo:
+ *Hola ---> aloH
+ *Osito--> otisO
+ *oso----> oso     //Es un palindromo
+ */
+public class App3Pilas {
+	
+	public static void main (String [] args) {
+		String cadenaEntrada = JOptionPane.showInputDialog("Inserta una cadena");
+		PilaEnlazada<Character> pila = new PilaEnlazada();
+		for (char c: cadenaEntrada.toCharArray()) {
+			pila.push(c);
+		}
+		String salida = cadenaEntrada + "    ---->    ";
+		while(!pila.isEmpty()) {
+			salida += pila.pop();
+		}
+		JOptionPane.showMessageDialog(null, salida);
+		
+		System.out.println("Esta la pila vacia? " + (pila.isEmpty()? "Si": "No"));
+	}
+
+}
